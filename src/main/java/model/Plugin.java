b4feb60505c5a30ca304fa21695c9bfa4fd7c5c0
gisselFdez <gisselita04@hotmail.com>
2016-02-03 19:48:36 +0100

    feat(model): create Dependency and Plugin model

---
+++ src/main/java/model/Plugin.java
@@ -0,0 +1,36 @@
+package model;
+
+public class Plugin {
+
+	private String groupId;
+	private String artifactId;
+	private String version;
+	private String configuration;
+	
+	public String getGroupId() {
+		return groupId;
+	}
+	public void setGroupId(String groupId) {
+		this.groupId = groupId;
+	}
+	public String getArtifactId() {
+		return artifactId;
+	}
+	public void setArtifactId(String artifactId) {
+		this.artifactId = artifactId;
+	}
+	public String getVersion() {
+		return version;
+	}
+	public void setVersion(String version) {
+		this.version = version;
+	}
+	public String getConfiguration() {
+		return configuration;
+	}
+	public void setConfiguration(String configuration) {
+		this.configuration = configuration;
+	}
+	
+	
+}
